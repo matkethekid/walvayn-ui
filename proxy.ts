915fc6b37bd3f6aa1496215ec91c4f6db6c9cb18
Mateja <markojovanovicc23@outlook.com>
2026-03-11 22:32:51 +0100

    optimized proxy for vercel

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,16 @@
 import createMiddleware from "next-intl/middleware";
+import { locales } from "./app/[locale]/layout";
 
 export default createMiddleware({
   locales: ["hr", "en"],
-  defaultLocale: "hr"
+  defaultLocale: "hr",
+  localePrefix: 'always' 
 });
 
 export const config = {
   matcher: [
     '/', 
-    '/(hr|en)/:path*',
+    '/(hr|en)/:path*', 
     '/((?!api|_next|_vercel|.*\\..*).*)'
   ]
 };

e5bd578d96009ee41d32c35ffc080701f5edc33d
Mateja <markojovanovicc23@outlook.com>
2026-03-13 14:15:47 +0100

    changed text for buttons

--- components/SaleComponent.tsx
+++ components/SaleComponent.tsx
@@ -107,10 +107,10 @@ const SaleComponent = () => {
           <div className="flex flex-row group items-center w-full relative">
             <Link href={"/trgovina"} className='relative overflow-hidden uppercase w-64 lg:w-80 h-16 lg:h-24 flex items-center justify-center bg-black border border-gray-100/10 text-white rounded-full text-2xl lg:text-3xl font-bold'>
               <span className='absolute transition-all duration-300 cubic-bounce group-hover:-translate-y-[350%]'>
-                pogledaj
+                iskoristi
               </span>
               <span className='absolute translate-y-[350%] transition-all duration-300 cubic-bounce group-hover:translate-y-0 whitespace-nowrap px-4'>
-                ponudu
+                priliku
               </span>
             </Link>
             <div className='relative overflow-hidden p-3 lg:p-10 rounded-full bg-black border border-gray-100/10 text-white h-16 lg:h-24 w-16 lg:w-24 flex items-center justify-center shrink-0'>

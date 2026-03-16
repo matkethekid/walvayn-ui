3d240a3fb4837f403e20a70ac2fa6e20b5466ccb
Mateja <markojovanovicc23@outlook.com>
2026-03-16 11:51:25 +0100

    shop & filters initialized

--- app/trgovina/page.tsx
+++ app/trgovina/page.tsx
@@ -1,14 +1,145 @@
 import Navbar from '@/components/Navbar';
+import localFont from 'next/font/local';
+import FilterComponent from '@/components/FilterComponent';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Inter } from 'next/font/google';
+
+const gudlakBold = localFont({
+  src: "../fonts/GudlakBold.woff2",
+  display: 'swap'
+});
+
+const inter = Inter({
+  subsets: ["latin-ext"],
+  weight: ["400", "600", "800"]
+});
+
+interface itemInCollection {
+  id: number;
+  name: string;
+  price: number;
+  onSale: boolean;
+  salePrice: number;
+  salePercent: number;
+  preferredUrl: string;
+  thumbnailImage: string;
+  sex: string;
+  collection: "zima" | "ljeto";
+};
+
+const itemsInCollection: itemInCollection[] = [
+  {
+    id: 0,
+    name: "Sportswear Tech Fleece",
+    price: 120,
+    onSale: false,
+    salePrice: 0,
+    salePercent: 0,
+    preferredUrl: "tech-fleece-nike-syna-world",
+    thumbnailImage: "/products/techfleece3.avif",
+    sex: "f",
+    collection: "zima"
+  },
+  {
+    id: 1,
+    name: "Tech Fleece Gray-Blue",
+    price: 120,
+    onSale: true,
+    salePrice: 100,
+    salePercent: 20,
+    preferredUrl: "tech-fleece-gray-blue",
+    thumbnailImage: "/products/techfleece2.avif",
+    sex: "m",
+    collection: "ljeto"
+  },
+  {
+    id: 2,
+    name: "Tech Fleece Olive",
+    price: 120,
+    onSale: false,
+    salePrice: 0,
+    salePercent: 0,
+    preferredUrl: "tech-fleece-olive",
+    thumbnailImage: "/products/techfleece4.avif",
+    sex: "m",
+    collection: "zima"
+  },
+  {
+    id: 3,
+    name: "Tech Fleece Oliveeee",
+    price: 120,
+    onSale: false,
+    salePrice: 0,
+    salePercent: 0,
+    preferredUrl: "tech-fleece-olive",
+    thumbnailImage: "/products/techfleece4.avif",
+    sex: "m",
+    collection: "zima"
+  },
+  {
+    id: 4,
+    name: "Tech Fleece Oliveaa",
+    price: 120,
+    onSale: false,
+    salePrice: 0,
+    salePercent: 0,
+    preferredUrl: "tech-fleece-olive",
+    thumbnailImage: "/products/techfleece4.avif",
+    sex: "m",
+    collection: "zima"
+  }
+];
 
 const page = () => {
+  const calculateDiscount = (price: number, percent: number) => {
+    if (percent <= 0) return price;
+    const discount = (price * percent) / 100;
+    return Math.round(price - discount);
+  };
   return (
     <section className='min-h-screen w-full'>
       <Navbar/>
-      <div className='w-full min-h-screen flex-1 bg-yellow-500 flex flex-row'>
-        <div className='w-[20%] h-screen bg-white flex flex-col p-5'>
-          <h1 className='text-3xl font-bold text-black'>Proizvodi (1570)</h1>
+      <div className='w-full min-h-screen flex-1 flex flex-col lg:flex-row'>
+        <aside className="lg:w-[20%] lg:sticky lg:top-20 p-5 lg:h-[calc(100vh-5rem)] overflow-y-auto">
+          <h1 className={`${gudlakBold.className} text-2xl text-black mb-10`}>Proizvodi (1570)</h1>
+          <FilterComponent />
+        </aside>
+        <div className='e-full lg:w-[80%] h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+          {itemsInCollection.map((item, index) => (
+              <div key={index} className="flex flex-col group cursor-pointer group relative">
+                <Link href={"/trgovina"}>
+                  <div className="relative aspect-4/5 w-full bg-[#f6f6f6] overflow-hidden">
+                    <div className="absolute inset-0 flex items-center justify-center z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none">
+                      <Image src={"/eye.svg"} alt="eye" width={65} height={65} className="opacity-[0.8]"/>
+                    </div>
+                    <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-[0.4] z-9 transition-opacity duration-500 pointer-events-none"></div>
+                      <Image src="/noise.svg" alt="noise" fill className="opacity-0 group-hover:opacity-[0.5] transition-opacity duration-500 pointer-events-none absolute inset-0 z-10 object-cover"/>
+                      <Image src={item.thumbnailImage} alt={item.name} fill className="object-cover" sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw" priority={index < 4}/>
+                    </div>
+                    <div className="mt-3 flex flex-col gap-1">
+                    <h3 className="text-black font-medium text-base leading-tight">
+                      {item.name}
+                    </h3>
+                    <h4 className="text-black font-medium text-base leading-tight">
+                      <span className={`${inter.className} font-semibold text-[#707072]`}>{item.sex === "m" ? "Muški" : "Ženski"}</span>
+                    </h4>
+                    <div className={`mt-2 font-medium text-base ${inter.className} font-bold`}>
+                      {item.onSale ? (
+                        <div className="flex gap-2">
+                          <span className={`font-bold ${inter.className}`}>{calculateDiscount(item.price, item.salePercent)}€</span>
+                          <span className={`text-[#707072] line-through font-bold ${inter.className}`}>{item.price}€</span>
+                          <span className={`text-[#007b55] ${inter.className} font-bold`}>{item.salePercent}% popust</span>
+                        </div>
+                        ) : (
+                        <span className={`font-bold ${inter.className}`}>{item.price}€</span>
+                      )}
+                    </div>
+                  </div>
+                </Link>
+              </div>
+          ))}
         </div>
-        <div className='w-[80%] h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-red-500'>aa</div>
       </div>
     </section>
   )
---
+++ components/FilterComponent.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useState } from "react";
+import { Inter } from "next/font/google";
+import { Checkbox } from "./ui/checkbox";
+import { ChevronDown, SlidersHorizontal } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Button } from "./ui/button";
+
+const inter = Inter({
+  subsets: ["latin-ext"],
+  weight: ["400", "700"]
+});
+
+const filters = [
+  {
+    id: "gender",
+    title: "Pol",
+    options: [
+      { label: "Muški", value: "muski" },
+      { label: "Ženski", value: "zenski" }
+    ]
+  },
+  {
+    id: "sale",
+    title: "Akcija",
+    options: [
+      { label: "Na akciji", value: "true" }
+    ]
+  },
+  {
+    id: "collection",
+    title: "Kolekcija",
+    options: [
+      { label: "Ljetnja", value: "ljetnja" },
+      { label: "Zimska", value: "zimska" }
+    ]
+  }
+];
+
+const FilterComponent = () => {
+  const [openSection, setOpenSection] = useState<string | null>("pol");
+  const [isHidden, setIsHidden] = useState<boolean>(true);
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const toggleSection = (section: string) => {
+    setOpenSection(prev => (prev === section ? null : section));
+  };
+
+  const toggleFilter = (filter: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    const values = params.getAll(filter);
+
+    if (values.includes(value)) {
+      const newValues = values.filter(v => v !== value);
+
+      params.delete(filter);
+      newValues.forEach(v => params.append(filter, v));
+    } else {
+      params.append(filter, value);
+    }
+
+    router.push(`?${params.toString()}`);
+  };
+
+  const isChecked = (filter: string, value: string) => {
+    return searchParams.getAll(filter).includes(value);
+  };
+  return (
+    <>
+      <Button onClick={() => setIsHidden(prevState => !prevState)} className='flex mb-10 justify-center items-center lg:hidden p-4 text-base rounded-full bg-black' aria-label='Otvori filter meni'>Filteri <SlidersHorizontal /></Button>
+      <div className={`${isHidden ? "hidden" : "block"} lg:block fixed inset-0 z-50 bg-white p-6 lg:relative lg:p-0 lg:z-0`}>
+        {filters.map(filter => (
+          <div key={filter.id} className={"flex flex-col border-gray-300 border border-l-transparent border-r-transparent border-b-transparent pt-5 pb-5"}>
+            <p className={`${inter.className} flex justify-between mb-3 font-bold`}>
+              {filter.title}
+              <ChevronDown className="cursor-pointer" onClick={() => toggleSection(filter.id)}/>
+            </p>
+            <ul className={openSection === filter.id ? "flex flex-col gap-2 transition-all duration-300" : "hidden transition-all duration-300"}>
+              {filter.options.map(option => (
+                <li key={option.value} className={`${inter.className} flex items-center gap-2`}>
+                  <Checkbox checked={isChecked(filter.id, option.value)} onCheckedChange={() => toggleFilter(filter.id, option.value)}/>
+                  <span>{option.label}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default FilterComponent;

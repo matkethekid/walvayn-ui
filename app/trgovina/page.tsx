52d740c9b967fa86e5d2745bfaeb85a67a61044f
Mateja <markojovanovicc23@outlook.com>
2026-03-16 17:40:01 +0100

    fixed FilterComponent issue with useSearchParams, fixed responsive

--- app/trgovina/page.tsx
+++ app/trgovina/page.tsx
@@ -4,6 +4,7 @@ import FilterComponent from '@/components/FilterComponent';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Inter } from 'next/font/google';
+import { Suspense } from 'react';
 
 const gudlakBold = localFont({
   src: "../fonts/GudlakBold.woff2",
@@ -103,41 +104,43 @@ const page = () => {
       <div className='w-full min-h-screen flex-1 flex flex-col lg:flex-row'>
         <aside className="lg:w-[20%] lg:sticky lg:top-20 p-5 lg:h-[calc(100vh-5rem)] overflow-y-auto">
           <h1 className={`${gudlakBold.className} text-2xl text-black mb-10`}>Proizvodi (1570)</h1>
-          <FilterComponent />
+          <Suspense fallback={<div>Učitavanje filtera...</div>}>
+            <FilterComponent />
+          </Suspense>
         </aside>
-        <div className='e-full lg:w-[80%] h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+        <div className='w-full p-3 lg:w-[80%] h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
           {itemsInCollection.map((item, index) => (
-              <div key={index} className="flex flex-col group cursor-pointer group relative">
-                <Link href={"/trgovina"}>
-                  <div className="relative aspect-4/5 w-full bg-[#f6f6f6] overflow-hidden">
-                    <div className="absolute inset-0 flex items-center justify-center z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none">
-                      <Image src={"/eye.svg"} alt="eye" width={65} height={65} className="opacity-[0.8]"/>
-                    </div>
-                    <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-[0.4] z-9 transition-opacity duration-500 pointer-events-none"></div>
-                      <Image src="/noise.svg" alt="noise" fill className="opacity-0 group-hover:opacity-[0.5] transition-opacity duration-500 pointer-events-none absolute inset-0 z-10 object-cover"/>
-                      <Image src={item.thumbnailImage} alt={item.name} fill className="object-cover" sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw" priority={index < 4}/>
-                    </div>
-                    <div className="mt-3 flex flex-col gap-1">
-                    <h3 className="text-black font-medium text-base leading-tight">
-                      {item.name}
-                    </h3>
-                    <h4 className="text-black font-medium text-base leading-tight">
-                      <span className={`${inter.className} font-semibold text-[#707072]`}>{item.sex === "m" ? "Muški" : "Ženski"}</span>
-                    </h4>
-                    <div className={`mt-2 font-medium text-base ${inter.className} font-bold`}>
-                      {item.onSale ? (
-                        <div className="flex gap-2">
-                          <span className={`font-bold ${inter.className}`}>{calculateDiscount(item.price, item.salePercent)}€</span>
-                          <span className={`text-[#707072] line-through font-bold ${inter.className}`}>{item.price}€</span>
-                          <span className={`text-[#007b55] ${inter.className} font-bold`}>{item.salePercent}% popust</span>
-                        </div>
-                        ) : (
-                        <span className={`font-bold ${inter.className}`}>{item.price}€</span>
-                      )}
-                    </div>
+            <div key={index} className="flex flex-col group cursor-pointer group relative mb-3">
+              <Link href={"/trgovina"}>
+                <div className="relative aspect-4/5 w-full bg-[#f6f6f6] overflow-hidden">
+                  <div className="absolute inset-0 flex items-center justify-center z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none">
+                    <Image src={"/eye.svg"} alt="eye" width={65} height={65} className="opacity-[0.8]"/>
                   </div>
-                </Link>
-              </div>
+                  <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-[0.4] z-9 transition-opacity duration-500 pointer-events-none"></div>
+                    <Image src="/noise.svg" alt="noise" fill className="opacity-0 group-hover:opacity-[0.5] transition-opacity duration-500 pointer-events-none absolute inset-0 z-10 object-cover"/>
+                    <Image src={item.thumbnailImage} alt={item.name} fill className="object-cover" sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw" priority={index < 4}/>
+                  </div>
+                  <div className="mt-3 flex flex-col gap-1">
+                  <h3 className="text-black font-medium text-base leading-tight">
+                    {item.name}
+                  </h3>
+                  <h4 className="text-black font-medium text-base leading-tight">
+                    <span className={`${inter.className} font-semibold text-[#707072]`}>{item.sex === "m" ? "Muški" : "Ženski"}</span>
+                  </h4>
+                  <div className={`mt-2 font-medium text-base ${inter.className} font-bold`}>
+                    {item.onSale ? (
+                      <div className="flex gap-2">
+                        <span className={`font-bold ${inter.className}`}>{calculateDiscount(item.price, item.salePercent)}€</span>
+                        <span className={`text-[#707072] line-through font-bold ${inter.className}`}>{item.price}€</span>
+                        <span className={`text-[#007b55] ${inter.className} font-bold`}>{item.salePercent}% popust</span>
+                      </div>
+                      ) : (
+                      <span className={`font-bold ${inter.className}`}>{item.price}€</span>
+                    )}
+                  </div>
+                </div>
+              </Link>
+          </div>
           ))}
         </div>
       </div>
--- components/FilterComponent.tsx
+++ components/FilterComponent.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Inter } from "next/font/google";
 import { Checkbox } from "./ui/checkbox";
-import { ChevronDown, SlidersHorizontal } from "lucide-react";
+import { ChevronDown, SlidersHorizontal, X } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "./ui/button";
 
@@ -72,14 +72,20 @@ const FilterComponent = () => {
   return (
     <>
       <Button onClick={() => setIsHidden(prevState => !prevState)} className='flex mb-10 justify-center items-center lg:hidden p-4 text-base rounded-full bg-black' aria-label='Otvori filter meni'>Filteri <SlidersHorizontal /></Button>
-      <div className={`${isHidden ? "hidden" : "block"} lg:block fixed inset-0 z-50 bg-white p-6 lg:relative lg:p-0 lg:z-0`}>
+      <div className={`${isHidden ? "hidden" : "block"} lg:block h-screen fixed inset-0 z-50 bg-white p-6 lg:relative lg:p-0 lg:z-0`}>
+        <div className="flex justify-between items-center mb-8 lg:hidden">
+          <h2 className="text-xl font-bold">Filteri</h2>
+          <button onClick={() => setIsHidden(true)} className="p-2">
+            <X size={28} />
+          </button>
+        </div>
         {filters.map(filter => (
           <div key={filter.id} className={"flex flex-col border-gray-300 border border-l-transparent border-r-transparent border-b-transparent pt-5 pb-5"}>
             <p className={`${inter.className} flex justify-between mb-3 font-bold`}>
               {filter.title}
               <ChevronDown className="cursor-pointer" onClick={() => toggleSection(filter.id)}/>
             </p>
-            <ul className={openSection === filter.id ? "flex flex-col gap-2 transition-all duration-300" : "hidden transition-all duration-300"}>
+            <ul className={openSection === filter.id ? "flex flex-col h-full gap-2 transition-all duration-300" : "hidden transition-all duration-300"}>
               {filter.options.map(option => (
                 <li key={option.value} className={`${inter.className} flex items-center gap-2`}>
                   <Checkbox checked={isChecked(filter.id, option.value)} onCheckedChange={() => toggleFilter(filter.id, option.value)}/>
